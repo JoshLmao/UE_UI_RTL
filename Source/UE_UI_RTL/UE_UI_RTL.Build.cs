753b81d5b6ebe17896b6b9e562e74ff77bbcda00
Josh Shepherd <josh_lmao@live.co.uk>
2025-02-02 11:41:00 +0000

    Init third person project template

---
+++ Source/UE_UI_RTL/UE_UI_RTL.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UE_UI_RTL : ModuleRules
+{
+	public UE_UI_RTL(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/UE_UI_RTL/UE_UI_RTLGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_UI_RTLGameMode.h"
+#include "UE_UI_RTLCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AUE_UI_RTLGameMode::AUE_UI_RTLGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
